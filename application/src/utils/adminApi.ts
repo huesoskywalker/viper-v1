bc505880764d225d86f38e9b668edcdf02e04ba1
Agustin Lavezzari <86160698+huesoskywalker@users.noreply.github.com>
2024-02-09 20:09:04 -0300

    Update adminApi.ts
    Remove || key

--- application/src/utils/adminApi.ts
+++ application/src/utils/adminApi.ts
@@ -7,8 +7,8 @@ import {
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-01"
 
 export const shopifyAdmin = shopifyApi({
-    apiKey: process.env.SHOPIFY_API_KEY || "69219fbe4ec26f26442a47b77f490b04",
-    apiSecretKey: "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", // Note: this is the API access token, NOT the API Secret Key
+    apiKey: process.env.SHOPIFY_API_KEY
+    apiSecretKey: 
     apiVersion: ApiVersion.January23 || LATEST_API_VERSION,
     isCustomStoreApp: true, // this MUST be set to true (default is false)
     scopes: process.env.SHOPIFY_SCOPES?.split(",") || [
@@ -23,5 +23,5 @@ export const shopifyAdmin = shopifyApi({
 
     privateAppStorefrontAccessToken:
         process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN ||
-        "259a9b2beca8d532d726aa6830179d11",
+        
 })
